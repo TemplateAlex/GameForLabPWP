805d46b4770a48209b3e16cfb9932f923c4fd728
TemplateAlex <mickeymouse.kz.ru@gmail.com>
2022-03-30 11:27:55 +0600

    Добавьте файлы проекта.

---
+++ GameForLabPWP/wwwroot/js/ScriptForParallax.js
@@ -0,0 +1,7 @@
+﻿function CreateParralax(event) {
+    this.querySelectorAll('.game-background').forEach(element => {
+        element.style.transform = 'translateX(' + event.clientX / 100 + 'px) translateY(' + event.clientY / 100 + 'px)';
+    });
+}
+
+document.addEventListener('mousemove', CreateParralax);
